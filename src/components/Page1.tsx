6f9bb8813c28c35bba01f4404368b7c74a80805e
Topias Saari <saari.topias@gmail.com>
2020-12-03 18:22:01 +0200

    first feature basis

--- src/components/Page1.tsx
+++ src/components/Page1.tsx
@@ -5,7 +5,30 @@ class Page1 extends React.Component {
   render() {
     return (
       <article>
-        <h1>PAGE 1</h1>
+        <div id="CO2container">
+          <div>
+            <div className="box" id="today">
+              <div>
+                <h2>TODAY</h2>
+                <h3>3.12.2020</h3>
+              </div>
+              <div>
+                <span>20 c02KG</span>
+              </div>
+            </div>
+            <div className="box" id="week">
+              <h2>WEEK</h2>
+            </div>
+          </div>
+          <div>
+            <div className="box" id="month">
+              <h2>MONTH</h2>
+            </div>
+            <div className="box" id="year">
+              <h2>YEAR</h2>
+            </div>
+          </div>
+        </div>
       </article>
     );
   }
