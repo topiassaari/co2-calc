6114c0d1e1feb6404c372417f18dcbffd071cddb
Topias Saari <43338418+topiassaari@users.noreply.github.com>
2021-02-16 14:15:34 +0200

    Updates

---
+++ precache-manifest.3f79fae9322aca8defa7fd93c0bfe156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297c6cec6d25e09f3f9468658859a363",
+    "url": "/co2-calc/index.html"
+  },
+  {
+    "revision": "f695e9ca5f7198c85915",
+    "url": "/co2-calc/static/css/main.9e27aa51.chunk.css"
+  },
+  {
+    "revision": "f7d7163b7b5f6d3a465d",
+    "url": "/co2-calc/static/js/2.f565fa34.chunk.js"
+  },
+  {
+    "revision": "512203927fde652151f92b0e52990cce",
+    "url": "/co2-calc/static/js/2.f565fa34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f695e9ca5f7198c85915",
+    "url": "/co2-calc/static/js/main.ba5d450b.chunk.js"
+  },
+  {
+    "revision": "3ab638618aaaadff1371",
+    "url": "/co2-calc/static/js/runtime-main.83dfa1e1.js"
+  },
+  {
+    "revision": "d2caf820cacaf0b7440c371a21d2f295",
+    "url": "/co2-calc/static/media/co2.d2caf820.png"
+  }
+]);
